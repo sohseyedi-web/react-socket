88ea68ca27f0ce58b98bba1f021a8c8b499b64b6
sohseyedi <sohseyedi@gmail.com>
2025-01-09 17:18:00 +0330

    chore(server):adding auth routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import authRoutes from './routes/AuthRoutes'
 
 dotenv.config();
 connectDB();
@@ -17,6 +18,8 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
