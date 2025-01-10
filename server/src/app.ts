c71209b0bc8642169949ed1a2da02182a218dedb
sohseyedi <sohseyedi@gmail.com>
2025-01-10 18:57:34 +0330

    chore(server): integrate socket.io for real-time communication and update routing structure

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,30 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
-import authRoutes from './routes/AuthRoutes'
+import messageRoutes from "./routes/MessageRoutes";
+import authRoutes from "./routes/AuthRoutes";
+import usersRoutes from "./routes/UsersRoutes";
+import socketHandler from "./socket/socket";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/chat", messageRoutes);
+app.use("/api/users", usersRoutes);
+socketHandler(io);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
