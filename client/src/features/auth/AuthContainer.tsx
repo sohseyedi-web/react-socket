41c65e86628d82bd4cdac0bc38469e4eabdac0c3
sohseyedi <sohseyedi@gmail.com>
2025-01-10 18:43:13 +0330

    feat(auth): add AuthContainer component for authentication layout

---
+++ client/src/features/auth/AuthContainer.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+const AuthContainer = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="lg:w-[35%] md:w-[70%] w-[90%] mx-auto p-4 bg-zinc-100 rounded-2xl border">
+        <h1 className="lg:text-3xl text-2xl my-2 text-center text-zinc-700 font-semibold">
+          سوکت اپ
+        </h1>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AuthContainer;
