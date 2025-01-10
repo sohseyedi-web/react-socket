2341478c81a810d252b0c1c062729b55ee4837fd
sohseyedi <sohseyedi@gmail.com>
2025-01-10 18:44:57 +0330

    feat(client): add ButtonAction component for enhanced button functionality

---
+++ client/src/ui/ButtonAction.tsx
@@ -0,0 +1,15 @@
+import { ButtonTypes } from "@/types";
+import Loading from "./Loading";
+
+const ButtonAction = ({ title, loading, type = "submit" }: ButtonTypes) => {
+  return (
+    <button
+      type={type}
+      className="bg-zinc-800 hover:bg-zinc-900 transition-all duration-300 font-semibold lg:text-lg w-full lg:h-[55px] h-[50px] rounded-2xl text-zinc-100 flex items-center justify-center cursor-pointer border-none"
+    >
+      {loading ? <Loading/> : title}
+    </button>
+  );
+};
+
+export default ButtonAction;
