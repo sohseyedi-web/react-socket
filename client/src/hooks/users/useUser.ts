75ceee13a411b97ba15bcbe02e26224132f9f75d
sohseyedi <sohseyedi@gmail.com>
2025-01-12 15:21:23 +0330

    feat(client): add custom hooks for user management and logout functionality

---
+++ client/src/hooks/users/useUser.ts
@@ -0,0 +1,37 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import api from "@/service/https";
+import { GET_ALL_USERS, GET_PROFILE_USER, LOGOUT_USER } from "@/service/urls";
+
+export const useDetailUser = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => api.get(GET_PROFILE_USER),
+    retry: false,
+  });
+
+  return { data, isLoading };
+};
+export const useAllUsers = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => api.get(GET_ALL_USERS),
+    retry: false,
+  });
+
+  return { data, isLoading };
+};
+
+export const useLogOut = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutateAsync: logOut, isPending } = useMutation({
+    mutationFn: () => api.post(LOGOUT_USER),
+    onSuccess: () => {
+      queryClient.removeQueries();
+      navigate("/join", { replace: true });
+    },
+  });
+
+  return { logOut, isPending };
+};
