107a6fb7642c9b5f91f5feadd0a391bfd62e279c
sohseyedi <sohseyedi@gmail.com>
2025-01-12 15:21:55 +0330

    feat(client): add ChatsContainer component for chat layout with sidebar

---
+++ client/src/ui/chatLayout/ChatsContainer.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+const ChatsContainer = () => {
+  return (
+    <section className="max-w-7xl mx-auto bg-gray-200 h-screen container" dir="ltr">
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 bg-gray-300 h-screen">
+          <Outlet />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ChatsContainer;
