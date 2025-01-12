a7f95a6d494203065af3a251a0c8dfbd07ca6c59
sohseyedi <sohseyedi@gmail.com>
2025-01-12 15:20:54 +0330

    feat(client): add Chats component for chat selection interface

---
+++ client/src/features/chats/Chats.tsx
@@ -0,0 +1,11 @@
+const Chats = () => {
+  return (
+    <section className="flex items-center justify-center h-screen">
+      <div className="px-3 py-1 bg-zinc-500 text-zinc-100 rounded-xl font-semibold">
+        Select a chat to start messaging
+      </div>
+    </section>
+  );
+};
+
+export default Chats;
