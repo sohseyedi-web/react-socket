8f1d89fa27a80068ecc0fa3b636394ec2b55d2c4
sohseyedi <sohseyedi@gmail.com>
2025-01-12 15:21:35 +0330

    chore(client): add new API endpoints for user logout and profile management

--- client/src/service/urls.ts
+++ client/src/service/urls.ts
@@ -1,2 +1,5 @@
 export const LOGIN_USER = `/auth/login`;
 export const REGISTER_USER = `/auth/register`;
+export const LOGOUT_USER = `/auth/logout`;
+export const GET_ALL_USERS = `/users/list`;
+export const GET_PROFILE_USER = `/users/profile`;
