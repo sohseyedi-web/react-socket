a1144dddaf05ff0bac0731ebf5a2c7224abcfc66
sohseyedi <sohseyedi@gmail.com>
2025-01-12 10:09:11 +0330

    chore(server):create get all user and user detail

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -1,11 +1,30 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import User from "../models/AuthModels";
+import jwt from "jsonwebtoken";
 
-export const getUsers = async (req: Request, res: Response) => {
+export const getAllUserData = async (req: Request, res: Response) => {
   try {
-    const users = await User.find({}, "username email");
-    res.json(users);
+    const user = await User.find();
+    res.status(200).json({ message: "success", user });
   } catch (error) {
-    res.status(500).json({ message: "❌ خطا در دریافت کاربران", error });
+    console.error(error);
+    res.status(401).json({ message: "توکن نامعتبر است" });
+  }
+};
+
+export const userProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.accessToken;
+  try {
+    const decoded = jwt.decode(token) as any;
+    const user = await User.findById(decoded.userId);
+    res.status(200).json(user);
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "توکن نامعتبر است" });
   }
 };
