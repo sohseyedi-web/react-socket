84b8b49f2ea8e2f9fe20a49b725351731133bc75
sohseyedi <sohseyedi@gmail.com>
2025-01-12 19:17:47 +0330

    chore(auth): redirect authenticated users to home on mount

--- client/src/features/auth/AuthContainer.tsx
+++ client/src/features/auth/AuthContainer.tsx
@@ -1,6 +1,17 @@
-import { ReactNode } from "react";
+import { useDetailUser } from "@/hooks/users/useUser";
+import { ReactNode, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContainer = ({ children }: { children: ReactNode }) => {
+  const { user } = useDetailUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/", { replace: true });
+    }
+  }, [user, navigate]);
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="lg:w-[35%] md:w-[70%] w-[90%] mx-auto p-4 bg-zinc-100 rounded-2xl border">
