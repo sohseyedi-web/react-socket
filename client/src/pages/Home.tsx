34adcb9a822a77e995838a97a7ec12ad867f7019
sohseyedi <sohseyedi@gmail.com>
2025-01-12 21:56:10 +0330

    refactor(client): remove user authentication check from Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,18 +1,6 @@
 import Chats from "@/features/chats/Chats";
-import { useDetailUser } from "@/hooks/users/useUser";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const { user } = useDetailUser();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!user) {
-      navigate("/join", { replace: true });
-    }
-  }, [user, navigate]);
-
   return <Chats />;
 };
 
