5cd6f45c3cffa29c768cc7316e4336c012355d33
sohseyedi <sohseyedi@gmail.com>
2025-01-12 15:20:40 +0330

    chore(client): restructure routing to include ChatsContainer and room navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import ChatsContainer from "./ui/chatLayout/ChatsContainer";
+import Chats from "./features/chats/Chats";
+import Room from "./features/room/Room";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ChatsContainer />}>
+          <Route path="" element={<Chats />} />
+          <Route path="/:roomId" element={<Room />} />
+        </Route>
         <Route path="/join" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
