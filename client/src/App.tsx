f6f94e2c33fcd4a0adfb8c51ebb0256f8e9b2300
sohseyedi <sohseyedi@gmail.com>
2025-01-12 21:55:44 +0330

    fix(client): update default route to render Home instead of Chats

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
-import ChatsContainer from "./ui/chatLayout/ChatsContainer";
-import Chats from "./features/chats/Chats";
-import Room from "./features/room/Room";
+import ChatsContainer from "@/ui/chatLayout/ChatsContainer";
+import Room from "@/features/room/Room";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ChatsContainer />}>
-          <Route path="" element={<Chats />} />
+          <Route path="" element={<Home />} />
           <Route path="/:roomId" element={<Room />} />
         </Route>
         <Route path="/join" element={<Login />} />
