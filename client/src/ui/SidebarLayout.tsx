643f9f694fac1db9d5e9d494bcc4e3adca251e0f
sohseyedi <sohseyedi@gmail.com>
2025-01-13 18:47:55 +0330

    style(client): update styles for improved layout and color contrast

--- client/src/ui/SidebarLayout.tsx
+++ client/src/ui/SidebarLayout.tsx
@@ -4,8 +4,8 @@ const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <aside
       className={`${
-        active ? "w-[25%] left-0 top-0" : "-left-28 w-0 top-0"
-      } fixed z-40 lg:relative bg-zinc-700 h-screen lg:h-auto border-r-2 space-y-3 transition-all duration-300`}
+        active ? "w-[18%] left-0 top-0" : "-left-28 w-0 top-0"
+      } fixed z-40 lg:relative bg-zinc-50 h-screen lg:h-auto border-r border-zinc-400 space-y-3 transition-all duration-300`}
     >
       {children}
     </aside>
