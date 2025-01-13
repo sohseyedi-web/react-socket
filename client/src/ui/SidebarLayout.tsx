6a12c7b97b11c8f11a756751c8d45ee3476de160
sohseyedi <sohseyedi@gmail.com>
2025-01-13 10:50:39 +0330

    chore(client): fix sidebar position

--- client/src/ui/SidebarLayout.tsx
+++ client/src/ui/SidebarLayout.tsx
@@ -4,7 +4,7 @@ const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <aside
       className={`${
-        active ? "w-[25%] right-0 top-0" : "-right-28 w-0 top-0"
+        active ? "w-[25%] left-0 top-0" : "-left-28 w-0 top-0"
       } fixed z-40 lg:relative bg-zinc-700 h-screen lg:h-auto border-r-2 space-y-3 transition-all duration-300`}
     >
       {children}
