bfae143bfcf00254222cf4e76afb77483746e689
sohseyedi <sohseyedi@gmail.com>
2025-01-13 22:07:03 +0330

    feat(client): add room API endpoints

--- client/src/service/urls.ts
+++ client/src/service/urls.ts
@@ -3,3 +3,5 @@ export const REGISTER_USER = `/auth/register`;
 export const LOGOUT_USER = `/auth/logout`;
 export const GET_ALL_USERS = `/users/list`;
 export const GET_PROFILE_USER = `/users/profile`;
+export const GET_ROOMS = `/rooms/lists`;
+export const CREATE_ROOMS = `/rooms/create`;
