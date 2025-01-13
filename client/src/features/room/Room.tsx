ce43757a7725aa4839295cc3cda53b244ddb70f3
sohseyedi <sohseyedi@gmail.com>
2025-01-13 18:47:44 +0330

    style(client): update styles for improved visual clarity

--- client/src/features/room/Room.tsx
+++ client/src/features/room/Room.tsx
@@ -9,17 +9,17 @@ const Room = () => {
   return (
     <section className="flex flex-col h-screen">
       <HeaderRoom />
-      <div className="flex-1 overflow-y-auto p-3">
+      <div className="flex-1 overflow-y-auto p-3 border-y border-zinc-400">
         <Messages userId={user?._id} />
       </div>
-      <form className="w-full bg-gray-950 py-2 px-3 flex items-center">
+      <form className="w-full bg-zinc-100 py-2 px-3 flex items-center">
         <input
           type="text"
           placeholder="Write a message..."
-          className="bg-transparent outline-none py-2 flex-1 text-white text-lg"
+          className="bg-transparent outline-none py-2 flex-1 text-zinc-800 text-lg"
         />
         <button>
-          <IoSend size={32} className="text-zinc-500 cursor-pointer" />
+          <IoSend size={32} className="text-zinc-700 cursor-pointer" />
         </button>
       </form>
     </section>
