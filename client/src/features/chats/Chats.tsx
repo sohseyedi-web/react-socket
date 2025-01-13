37a49cf6c2b668db5b926ba81657cf2ec4c299fe
sohseyedi <sohseyedi@gmail.com>
2025-01-13 17:02:43 +0330

    chore(client): enhance Chats component with welcome message and icon display

--- client/src/features/chats/Chats.tsx
+++ client/src/features/chats/Chats.tsx
@@ -1,9 +1,25 @@
+import { LuMessageSquare } from "react-icons/lu";
 
 const Chats = () => {
   return (
     <section className="flex items-center justify-center h-screen">
-      <div className="px-3 py-1 bg-zinc-500 text-zinc-100 rounded-xl font-semibold">
-        Select a chat to start messaging
+      <div className="max-w-md text-center space-y-6">
+        {/* Icon Display */}
+        <div className="flex justify-center gap-4 mb-4">
+          <div className="relative">
+            <div
+              className="w-16 h-16 rounded-2xl flex items-center
+             justify-center animate-bounce"
+            >
+              <LuMessageSquare className="w-8 h-8 text-zinc-700" />
+            </div>
+          </div>
+        </div>
+
+        <h2 className="text-2xl font-bold">Welcome to SockeTapp!</h2>
+        <p className="text-sm">
+          Select a conversation from the sidebar to start chatting
+        </p>
       </div>
     </section>
   );
