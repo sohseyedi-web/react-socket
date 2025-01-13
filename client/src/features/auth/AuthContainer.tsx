961684270eec12a294b026fb6a8b65465719c3b2
sohseyedi <sohseyedi@gmail.com>
2025-01-13 10:49:44 +0330

    chore(client): update header text

--- client/src/features/auth/AuthContainer.tsx
+++ client/src/features/auth/AuthContainer.tsx
@@ -5,7 +5,7 @@ const AuthContainer = ({ children }: { children: ReactNode }) => {
     <div className="flex items-center justify-center h-screen">
       <div className="lg:w-[35%] md:w-[70%] w-[90%] mx-auto p-4 bg-zinc-100 rounded-2xl border">
         <h1 className="lg:text-3xl text-2xl my-2 text-center text-zinc-700 font-semibold">
-          سوکت اپ
+          Socket App
         </h1>
         {children}
       </div>
