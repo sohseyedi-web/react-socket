dffa47bbcf92aa4f7e5584457526135af04fb33a
sohseyedi <sohseyedi@gmail.com>
2025-01-13 10:11:53 +0330

    refactor(client): remove authentication logic from Chats component

--- client/src/features/chats/Chats.tsx
+++ client/src/features/chats/Chats.tsx
@@ -1,17 +1,5 @@
-import { useAuthorize } from "@/hooks/users/useAuthorize";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 const Chats = () => {
-  const { isAuthenticated } = useAuthorize();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate("/join", { replace: true });
-    }
-  }, [isAuthenticated, navigate]);
-
   return (
     <section className="flex items-center justify-center h-screen">
       <div className="px-3 py-1 bg-zinc-500 text-zinc-100 rounded-xl font-semibold">
