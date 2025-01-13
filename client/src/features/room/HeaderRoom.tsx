38d8a4364490bae3a3e4d1e25ccef08766e51607
sohseyedi <sohseyedi@gmail.com>
2025-01-13 18:47:11 +0330

    chore(client): update HeaderRoom component styles for improved visual consistency

--- client/src/features/room/HeaderRoom.tsx
+++ client/src/features/room/HeaderRoom.tsx
@@ -12,22 +12,22 @@ const HeaderRoom = () => {
   );
 
   return (
-    <header className="lg:px-3 px-2 py-2 flex items-center border-b justify-between">
-      <div className="flex items-center gap-x-2 bg-white">
+    <header className="lg:px-3 px-2 py-2 flex items-center justify-between">
+      <div className="flex items-center gap-x-2">
         <div className="flex items-center">
           <RiMenu2Line
             size={25}
-            className="text-zinc-300 cursor-pointer lg:hidden flex mr-3"
+            className="text-zinc-500 cursor-pointer lg:hidden flex mr-3"
           />
           <img
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
             alt=""
-            className="size-9 rounded-2xl lg:hidden flex"
+            className="size-10 rounded-2xl lg:hidden flex"
           />
         </div>
         <div className="flex flex-col">
-          <h4 className="text-lg font-semibold">{findUser?.username}</h4>
-          <span className="text-sm">online</span>
+          <h4 className="text-lg font-medium">{findUser?.username}</h4>
+          <span className="text-sm text-zinc-400">online</span>
         </div>
       </div>
       <div className="flex gap-x-2">
