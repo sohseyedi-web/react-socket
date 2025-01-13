2eff35c38470cc46f82b7dcfbfa4ea31823fe0b3
sohseyedi <sohseyedi@gmail.com>
2025-01-13 10:27:47 +0330

    chore(client): enhance Loading component with full-screen overlay option

--- client/src/ui/Loading.tsx
+++ client/src/ui/Loading.tsx
@@ -1,5 +1,33 @@
-const Loading = () => {
-  return (
+const Loading = ({ full = false }: { full?: boolean }) => {
+  return full ? (
+    <div
+      id="loading-overlay"
+      dir="ltr"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(0,0,0,0.9)]"
+    >
+      <svg
+        className="animate-spin h-8 w-8 text-white mr-3"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      <span className="text-white text-3xl font-bold">Loading...</span>
+    </div>
+  ) : (
     <div className="flex items-center space-x-2">
       <span className="sr-only">Loading...</span>
       <div className="size-3 bg-zinc-200 rounded-full animate-bounce [animation-delay:-0.3s]" />
