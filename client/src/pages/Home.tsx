d5ba48ccf43f79e10fd219ca7e2f5c7eeeb7c1a2
sohseyedi <sohseyedi@gmail.com>
2025-01-13 19:10:29 +0330

    chore(client): add responsive menu toggle to Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,22 @@
 import Chats from "@/features/chats/Chats";
+import { useResponsiveStore } from "@/store/useStore";
+import { RiMenu2Line } from "react-icons/ri";
 
 const Home = () => {
-  return <Chats />;
+  const { active, setActive } = useResponsiveStore();
+
+  return (
+    <section className="relative">
+      <div className="absolute top-4 left-5 lg:hidden flex">
+        <RiMenu2Line
+          onClick={() => setActive(!active)}
+          size={28}
+          className="text-zinc-500 cursor-pointer"
+        />
+      </div>
+      <Chats />
+    </section>
+  );
 };
 
 export default Home;
