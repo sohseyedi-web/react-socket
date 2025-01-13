1a7885117ac9d020c95dc6af26b6994fd84a8e63
sohseyedi <sohseyedi@gmail.com>
2025-01-13 10:16:49 +0330

    fix(server): enhance userProfile error handling and token validation

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -18,13 +18,25 @@ export const userProfile = async (
   next: NextFunction
 ) => {
   const token = req.cookies.accessToken;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   try {
     const decoded = jwt.decode(token) as any;
+    if (!decoded || !decoded.userId) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
     const user = await User.findById(decoded.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
     res.status(200).json(user);
-    next();
   } catch (error) {
     console.error(error);
-    res.status(401).json({ message: "توکن نامعتبر است" });
+    return res.status(500).json({ message: "Internal Server Error" });
   }
+  next();
 };
