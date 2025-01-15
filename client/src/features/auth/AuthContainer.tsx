0d44aa14a34eb9a675aa631acbb8ae63853f4f6b
sohseyedi <sohseyedi@gmail.com>
2025-01-15 18:58:23 +0330

    chore(client): update AuthContainer styles with gradient background and new color scheme

--- client/src/features/auth/AuthContainer.tsx
+++ client/src/features/auth/AuthContainer.tsx
@@ -2,9 +2,9 @@ import { ReactNode } from "react";
 
 const AuthContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="lg:w-[35%] md:w-[70%] w-[90%] mx-auto p-4 bg-zinc-100 rounded-2xl border">
-        <h1 className="lg:text-3xl text-2xl my-2 text-center text-zinc-700 font-semibold">
+    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-[#1D1D1D] to-[#2d2d2d]">
+      <div className="lg:w-[35%] md:w-[70%] w-[90%] mx-auto p-4 bg-[#2A3335] rounded-2xl">
+        <h1 className="lg:text-3xl text-2xl my-2 text-center text-[#f6f6f6] font-semibold">
           Socket App
         </h1>
         {children}
