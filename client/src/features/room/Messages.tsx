e4360f8e393f8fe8b8a79a7c682e24c290d04e2f
sohseyedi <sohseyedi@gmail.com>
2025-01-15 18:59:14 +0330

    style(client): update Messages component styles

--- client/src/features/room/Messages.tsx
+++ client/src/features/room/Messages.tsx
@@ -42,13 +42,13 @@ const Messages = ({ currentUser, messages }: MessageProps) => {
               className={`px-2 py-3 rounded-md shadow-md min-w-[100px] max-w-[200px] break-words flex flex-col ${
                 msg.sender === currentUser
                   ? "bg-blue-500 text-white rounded-tl-2xl"
-                  : "bg-zinc-700 text-white rounded-tr-2xl"
+                  : "bg-neutral-900 text-white rounded-tr-2xl"
               }`}
             >
-              <span className="text-right">{msg.message}</span>
+              <span className={msg.sender === currentUser ? "text-right" : "text-left"}>{msg.message}</span>
             </div>
             <span
-              className={`text-xs font-semibold text-zinc-700 ${
+              className={`text-xs font-semibold text-zinc-400 ${
                 msg.sender === currentUser ? "text-right" : "text-left"
               }  mt-1`}
             >
